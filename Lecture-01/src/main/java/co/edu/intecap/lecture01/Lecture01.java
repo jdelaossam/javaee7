b3afdc69775bdec1ee86f7a364ba14399bfad029
LAPTOP-FPCSFC6I\Cesar <ca07hg03>
2016-10-28 22:24:23 -0500

    Lecture01 Hello World App

---
+++ Lecture-01/src/main/java/co/edu/intecap/lecture01/Lecture01.java
@@ -0,0 +1,22 @@
+/*
+ * Intecap
+ * JAVA Learning center
+ * .
+ */
+package co.edu.intecap.lecture01;
+
+/**
+ *
+ * @author cesar.hoyos@gmail.com
+ */
+public class Lecture01 {
+
+    /**
+     * Lecture01 Hello World
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome from Lecture 1!");
+    }
+
+}
